66df10559f5f497c981e112600457e69717a9fb6
zora <270529732@qq.com>
2020-10-08 16:41:22 +0800

    动画

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "@assets/scss/reset.scss";
 import Common from "@comp";
-import animated from "animate.css";
 
-Vue.use(animated);
 Vue.config.productionTip = false;
 Vue.use(Common);
 new Vue({
